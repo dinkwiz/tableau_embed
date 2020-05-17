90112f1935da0d40e5ca0ad5a4b3442bc8e7eb92
Dink Wiz <56519900+dinkwiz@users.noreply.github.com>
2020-05-17 14:36:01 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,49 @@
-Console.log(‘I am here’);
-Let viz
-Const containerdiv = document.getelementbyid(“vizcontainer”)
-Const url = "https://public.tableau.com/views/LarryLefttheLightsOn/Dashboard1?:display_count=y&:origin=viz_share_link"
-Const options = {Hide tabs: true,}
-Function initviz() {viz = new tableau.viz(containerdiv, url, options}
-Document.addeventlistener(‘DOMcontentloaded’),initviz);
+console.log('Is this working?');
+
+let viz;
+
+//Add Share Link to Tableau Public in here
+const url = "https://public.tableau.com/views/Squirrels_15746293266160/Dashboard1?:display_count=y&:origin=viz_share_link";
+
+const vizContainer = document.getElementById('vizContainer');
+const options = {
+    hideTabs: true,
+    height: 1000,
+    width: 1200,
+    onFirstInteraction: function() {
+        workbook = viz.getWorkbook();
+        activeSheet = workbook.getActiveSheet();
+        console.log("My dashboard is interactive");
+    }
+};
+
+//create a function to generate the viz element
+function initViz() {
+    console.log('Executing the initViz function!');
+    viz = new tableau.Viz(vizContainer, url, options);
+}
+
+// run the initViz function when the page loads
+document.addEventListener("DOMContentLoaded", initViz);
+
+const exportPDF = document.getElementById('exportPDF');
+const exportImage = document.getElementById('exportImage');
+
+
+//click on the pdf button to generate pdf of dashboard
+function generatePDF() {
+    viz.showExportPDFDialog()
+}
+
+exportPDF.addEventListener("click", function () {
+    generatePDF();
+  });
+
+//click on image to generate image of dashboard
+function generateImage() {
+    viz.showExportImageDialog()
+}
+
+exportImage.addEventListener("click", function () {
+    generateImage();
+  });
